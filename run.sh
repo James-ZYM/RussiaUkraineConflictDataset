5e4f340c06446318b83aaf7ccf1082a51e91f63c
KathrineSN <KathrineSN@live.dk>
2022-11-30 10:22:23 +0100

    added tweet example

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/bash
+# prettier outputs
+GREEN='\033[1;32m'
+NC='\033[0m'
+# update apt-get
+sudo apt-get update
+# this assumes python3.9 - change if different
+sudo apt-get install python3.9-dev -y
+# upgrade pip; install BERTopic
+pip install --upgrade pip
 pip install bertopic
+# done
+echo -e "[${GREEN}INFO:${NC}] Everything installed!"
+
+#in terminal write: bash NLP\ examproject\ \(293386\)/NLPexam/run.sh
