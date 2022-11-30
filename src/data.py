314be9f916de9883c1c049aeef1de7601ef7b0f3
anrgaard <anfonager@gmail.com>
2022-11-30 11:39:22 +0100

    added date, subreddit and post type in the pd dataframe

--- src/data.py
+++ src/data.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import csv
 import re
+from langdetect import detect
 
 def get_files():
     data_dirs = [os.path.join(".." , "Comments"), os.path.join(".." ,"Submissions")]
@@ -22,15 +23,16 @@ def file_to_df(file_list):
     list = []
     for file in file_list:
         with open(file, 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                text1 = row[14] # body
-                #text2 = row[15] # body_sha_1 
-                subreddit = row[41] # 40 for those where body = False
-                timestamp = row[47]
-                data_list = [text1, subreddit, timestamp]
+        split_file_name = file.split("/")
+        r = split_file_name[3].split(".")
+        reader = csv.reader(f)
+        for row in reader:
+            text1 = row[14] # body
+            text2 = row[15] # body_sha_1
+            subreddit = r[0]
+            post_type = split_file_name[1]
+            date = split_file_name[2]
+            data_list = [text1, text2, subreddit, post_type, date]
                 list.append(data_list)
-                #list.append(text2)
         data = pd.DataFrame(list)
     return data
-    
