663e2a611be63a37a0bb57aa01cd5df0f6710119
KathrineSN <KathrineSN@live.dk>
2022-12-07 22:27:02 +0100

    changes in run

--- run.sh
+++ run.sh
@@ -3,13 +3,20 @@
 GREEN='\033[1;32m'
 NC='\033[0m'
 # update apt-get
-sudo apt-get update
+#sudo apt-get update
 # this assumes python3.9 - change if different
-sudo apt-get install python3.9-dev -y
-# upgrade pip; install BERTopic
+#sudo apt-get install python3.9-dev -y
+
+# creating the virtual environment
+pip install virtualenv
+virtualenv -p /usr/bin/python3.9 nlp-env
+# activate the environment
+source nlp-env/bin/activate
+
 pip install --upgrade pip
 pip install bertopic
+pip install langdetect
+
 # done
 echo -e "[${GREEN}INFO:${NC}] Everything installed!"
 
-#in terminal write: bash NLP\ examproject\ \(293386\)/NLPexam/run.sh
