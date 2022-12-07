d02a775a89e8a998f067217a210a5553725aac8b
anrgaard <anfonager@gmail.com>
2022-12-07 11:49:08 +0100

    tidied up data.py and main.py

--- src/data.py
+++ src/data.py
@@ -31,12 +31,23 @@ def get_files(current_path: str, data_dirs: list) -> list:
     return file_list
 
 def file_to_df(file_list: list) -> pd.DataFrame and list:
+
+    """
+        Args:
+        file_list (list): list of strings containing all file names with their path
+
+    Returns:
+        pd.DataFrame: 
+        list: list of lists with filenames of the files that did not read into the pd.DataFrame and the corresponding error.
+    """
+
     list = []
     i = 0
     exceptions = []
     for file in file_list:
         i += 1 
         try:
+            # open file and append a list of two text columns, the subreddit column, the post type and the date to list
             with open(file, 'r') as f:
                 split_file_name = file.split("/")
                 r = split_file_name[3].split(".")
@@ -52,36 +63,46 @@ def file_to_df(file_list: list) -> pd.DataFrame and list:
                         date = split_file_name[2]
                     data_list = [text1, text2, subreddit, post_type, date]
                     list.append(data_list)
+                # some print to see progress
                 if subreddit == "war":
                     print(f'Finished processing file number {i} of {len(file_list)}!')
                     print(f'So far I have identified {len(exceptions)} exception files.', '\n')
+        # if file is unable to read append it to list of exceptions
         except Exception as e: exceptions.append([file, str(e)])
+    
+    # create pd.DataFrame
     data = pd.DataFrame(list)
+        # rename columns
     data.columns = ["body", "body_sha1", "subreddit", "post_type", "date"]
+        # temp data to be able to merge the two text columns
     temp_data = data.loc[(data['body'] == "True") | (data['body'] == "False")]
     temp_data = temp_data.drop(columns = "body")
     temp_data = temp_data.rename(columns = {'body_sha1':'body'})
     data = data.drop(columns="body_sha1")
     final_data = pd.concat([temp_data, data])
+        # remove duplicate rows (from entries where text was in body_sha1)
     final_data['index1'] = final_data.index
     final_data = final_data.drop_duplicates(subset='index1')
     
     # detect language of comment
-    
-    # record start time
+        # record start time
     start = time.time()
-    print("Almost finished. I just need to detect the languages in your dataset. This should take a good while, so this is a good time to get up and stretch your legs.", "\n", "For reference, this process took ~3 minutes running 10 files on 8 CPUs.")
+    i = 0
+    print("Beginning language detection...", "\n")
     final_data['language'] = pd.Series(dtype='str')
     for row in range(len(final_data)):
+        i += 1
         try:
             final_data.iloc[row, 5] = detect(final_data.iloc[row, 0])
         except: 
             final_data.iloc[row, 5] = "none"
-    # record end time
+        if i % 100000 == 0: 
+            print(f'Finished detecting language in {i} out of {len(final_data)} cases!')
+        # record end time
     end = time.time()
  
-    # print the difference between start and end time in milli. secs
-    print(f"The time of execution of the language detection was : {end-start} s")
+    # print the difference between start and end time in secs
+    print(f"The time of execution of the language detection was : {(end-start)/60} mins")
     
     # drop rows where comment has been removed and NAs
     final_data = final_data.drop(final_data[final_data.body == "[removed]"].index)
--- src/main.py
+++ src/main.py
@@ -4,24 +4,27 @@ import csv
 from data import *
 import pickle
 
+# running this script took 7.65 hrs on all comments (9225973 rows) on 64 CPUs
 
-cwd = os.path.dirname(os.path.abspath(__file__))
+def main():
+    cwd = os.path.dirname(os.path.abspath(__file__))
 
-dirs = [os.path.join(".." , "Comments")]
-file_list = get_files(current_path = cwd, data_dirs = dirs)
+    dirs = [os.path.join(".." , "Comments")]
+    file_list = get_files(current_path = cwd, data_dirs = dirs)
 
-file_list_sub = file_list[:10]
+    data, exception_files = file_to_df(file_list)
 
-data, exception_files = file_to_df(file_list_sub)
-
-outfile_csv = os.path.join("..", "data", "formatted_data.csv")
-outfile_pickle = os.path.join("..", "data", "formatted_data.pkl")
-data.to_csv(outfile_csv) 
-data.to_pickle(outfile_pickle)
+    outfile_csv = os.path.join("..", "data", "formatted_data.csv")
+    outfile_pickle = os.path.join("..", "data", "formatted_data.pkl")
+    data.to_csv(outfile_csv) 
+    data.to_pickle(outfile_pickle)
 
 
-outfile = os.path.join("..", "data", "exception_files.txt")
-with open(outfile, "w") as f:
-    for item in exception_files:
-        # write each item on a new line
-        f.write("%s\n" % item)
+    outfile = os.path.join("..", "data", "exception_files.txt")
+    with open(outfile, "w") as f:
+        for item in exception_files:
+            # write each item on a new line
+            f.write("%s\n" % item)
+
+if __name__ == "__main__":
+    main()
