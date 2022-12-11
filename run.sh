3790f057aac4dabea211da7b5f2a8a1e725b0912
KathrineSN <KathrineSN@live.dk>
2022-12-11 10:49:42 +0100

    added attempt on dynamic modeling

--- run.sh
+++ run.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/bash
 
 # set up virtual environment
-python3.9 -m venv env
-source ./env/bin/activate
+sudo apt-get install python3-venv
+python3.9 -m venv nlp-env
+source ./nlp-env/bin/activate
 
 # prettier outputs
 GREEN='\033[1;32m'
 NC='\033[0m'
 # update apt-get
-#sudo apt-get update
+sudo apt-get update
 # this assumes python3.9 - change if different
-#sudo apt-get install python3.9-dev -y
+sudo apt-get install python3.9-dev -y
 
 # creating the virtual environment
-pip install virtualenv
-virtualenv -p /usr/bin/python3.9 nlp-env
+#pip install virtualenv
+#virtualenv -p /usr/bin/python3.9 nlp-env
 # activate the environment
-source nlp-env/bin/activate
+#source nlp-env/bin/activate
 
 pip install --upgrade pip
 pip install bertopic
